80be2d42d74b44cd16f4f4e99df33900a44dc503
John Waweru <john.waweru@nathandigital.com>
2025-02-24 17:37:51 +0300

    started working on authentication

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signUp = async (req, res, next) => {
+    // Implement signup logic
+}
+
+export const signIn = async (req, res, next) => {
+    // Implement signin logic
+}
+
+export const signOut = async (req, res, next) => {
+    // Implement signout logic
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
+import { signUp } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-    res.send({ title: "Sign Up" });
-});
+authRouter.post("/sign-up", signUp);
 
 authRouter.post("/sign-in", (req, res) => {
     res.send({ title: "Sign In" });
