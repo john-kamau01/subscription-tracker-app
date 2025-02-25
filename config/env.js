f6ca64efe4c71a7cdb66f8e387843d8630f828dd
root <root@NDK205-JohnWaweru-win.localdomain>
2025-02-25 16:51:33 +0300

    added arcject for rate limiting

--- config/env.js
+++ config/env.js
@@ -2,6 +2,4 @@ import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
-
-// token -> eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2JkYmE3MmY2MzM5YTFiMWNmMTY3ZTciLCJpYXQiOjE3NDA0ODcyODIsImV4cCI6MTc0MDU3MzY4Mn0._6GEKDK3rmf25YpMmxYOx1Ty0ipb1gQYK5d-vxduFrA
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY, ARCJET_ENV } = process.env;
